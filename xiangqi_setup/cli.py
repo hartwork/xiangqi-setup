44c3d8dcd22d10c4dff1e74ad51fa04a9dc03378
Sebastian Pipping <sebastian@pipping.org>
2014-08-03 02:22:18 +0200

    Split --width into --width-px and --width-cm

--- xiangqi_setup/cli.py
+++ xiangqi_setup/cli.py
@@ -18,6 +18,13 @@ def check(options):
         print('ERROR: Piece scale must be larger than zero and greater or equal one.', file=sys.stderr)
         sys.exit(1)
 
+    if options.width_centimeter is not None:
+        options.width_pixel = cm_to_pixel(options.width_centimeter)
+    delattr(options, 'width_centimeter')
+    if options.width_pixel is None:
+        options.width_pixel = cm_to_pixel(7.0)
+
+
 def run(options):
     pieces_to_put = list(iterate_wxf_tokens(options.input_file))
     compose_svg(pieces_to_put, options)
@@ -27,7 +34,8 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--board', dest='board_theme_dir', metavar='DIRECTORY', default=os.path.join('themes', 'board', 'commons_xiangqi_board_2008_bw_thin'))
     parser.add_argument('--pieces', dest='pieces_theme_dir', metavar='DIRECTORY', default=os.path.join('themes', 'pieces', 'commons_xiangqi_pieces_print_2010_bw_heavy'))
-    parser.add_argument('--width', dest='width_pixel', metavar='PIXEL', type=float, default=cm_to_pixel(7.0))
+    parser.add_argument('--width-px', dest='width_pixel', metavar='PIXEL', type=float)
+    parser.add_argument('--width-cm', dest='width_centimeter', metavar='CENTIMETER', type=float)
     parser.add_argument('--scale-pieces', dest='piece_scale', metavar='FACTOR', type=float, default=0.9)
     parser.add_argument('--debug', action='store_true')
     parser.add_argument('input_file', metavar='INPUT_FILE')

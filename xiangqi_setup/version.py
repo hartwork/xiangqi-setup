7f975e2f7eb3317e3029eb732e5fee3bafa93d58
Sebastian Pipping <sebastian@pipping.org>
2021-01-10 18:59:22 +0100

    Bump version to 1.7.0

--- xiangqi_setup/version.py
+++ xiangqi_setup/version.py
@@ -1,4 +1,4 @@
 # Copyright (C) 2015 Sebastian Pipping <sebastian@pipping.org>
 # Licensed under GNU Affero General Public License version 3.0 or later
 
-VERSION_STR = '1.6.0'
+VERSION_STR = '1.7.0'

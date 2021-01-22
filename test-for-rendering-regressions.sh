d407f3627c4b7ae1d264b97c3c64bfc4cd3edda0
Sebastian Pipping <sebastian@pipping.org>
2021-01-22 20:00:36 +0100

    Actions: Stop doing colorspace normalization

--- test-for-rendering-regressions.sh
+++ test-for-rendering-regressions.sh
@@ -20,13 +20,9 @@ render_and_compare() {
     local prefix_actual="tests/actual-${theme_type}-${theme_name}"
     local prefix_expected="tests/expected-${theme_type}-${theme_name}"
     local prefix_difference="tests/difference-${theme_type}-${theme_name}"
-    local colorspace='-define png:color-type=6 -define png:bit-depth=8'  # i.e. 32bit RGBA
 
     xiangqi-setup "--${theme_type}" "${theme_name}" doc/demo.wxf "${prefix_actual}".svg > /dev/null
-    # NOTE: We normalize both images to the same color space and depth before comparison
-    #       so that only a "true difference in color value" can cause a diff here.
-    convert ${colorspace} -verbose -background none "${prefix_actual}".{svg,png}
-    convert ${colorspace} "${prefix_expected}".png{,}  # in-place
+    convert -verbose -background none "${prefix_actual}".{svg,png}
     assert_images_identical {"${prefix_expected}","${prefix_actual}","${prefix_difference}"}.png
 
     rm "${prefix_actual}".{png,svg}

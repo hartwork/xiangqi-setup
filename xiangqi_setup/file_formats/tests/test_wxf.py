1a750477536a8654d1e3028a1ef07aa3db30b301
Sebastian Pipping <sebastian@pipping.org>
2021-01-27 18:04:18 +0100

    tests: Cover _Board._calculate_destination_of_move

--- xiangqi_setup/file_formats/tests/test_wxf.py
+++ xiangqi_setup/file_formats/tests/test_wxf.py
@@ -5,9 +5,9 @@ from unittest import TestCase
 
 from parameterized import parameterized
 
-from xiangqi_setup.file_formats.wxf import _PlayerRelativeView
+from xiangqi_setup.file_formats.wxf import _PlayerRelativeView, _Board
 from xiangqi_setup.parties import RED, BLACK
-from xiangqi_setup.pieces import PutPiece, ELEPHANT
+from xiangqi_setup.pieces import PutPiece, ELEPHANT, CHARIOT, CANNON, HORSE, ADVISOR, KING, PAWN
 
 
 class PlayerRelativeViewTest(TestCase):
@@ -43,3 +43,80 @@ class PlayerRelativeViewTest(TestCase):
         expected_piece_furthest_up_the_board = PutPiece(party, piece, *expected_furthest_up_the_board_x_y)
         actual_piece_furthest_up_the_board = sorted(x_y_put_pieces, key=view.further_up_the_board_sort_key)[-1]
         self.assertEqual(actual_piece_furthest_up_the_board, expected_piece_furthest_up_the_board)
+
+
+class BoardTest(TestCase):
+    @parameterized.expand([
+        (CHARIOT, RED, (3, 5), '+', '3', (3, 8)),
+        (CHARIOT, BLACK, (3, 5), '+', '3', (3, 2)),
+        (CHARIOT, RED, (3, 5), '-', '3', (3, 2)),
+        (CHARIOT, BLACK, (3, 5), '-', '3', (3, 8)),
+        (CHARIOT, RED, (3, 5), '.', '1', (8, 5)),
+        (CHARIOT, RED, (3, 5), '.', '9', (0, 5)),
+        (CHARIOT, BLACK, (3, 5), '.', '1', (0, 5)),
+        (CHARIOT, BLACK, (3, 5), '.', '9', (8, 5)),
+
+        (HORSE, RED, (2, 4), '+', '9', (0, 5)),
+        (HORSE, RED, (2, 4), '+', '8', (1, 6)),
+        (HORSE, RED, (2, 4), '+', '6', (3, 6)),
+        (HORSE, RED, (2, 4), '+', '5', (4, 5)),
+        (HORSE, BLACK, (2, 4), '+', '1', (0, 3)),
+        (HORSE, BLACK, (2, 4), '+', '2', (1, 2)),
+        (HORSE, BLACK, (2, 4), '+', '4', (3, 2)),
+        (HORSE, BLACK, (2, 4), '+', '5', (4, 3)),
+        (HORSE, RED, (2, 4), '-', '9', (0, 3)),
+        (HORSE, RED, (2, 4), '-', '8', (1, 2)),
+        (HORSE, RED, (2, 4), '-', '6', (3, 2)),
+        (HORSE, RED, (2, 4), '-', '5', (4, 3)),
+        (HORSE, BLACK, (2, 4), '-', '1', (0, 5)),
+        (HORSE, BLACK, (2, 4), '-', '2', (1, 6)),
+        (HORSE, BLACK, (2, 4), '-', '4', (3, 6)),
+        (HORSE, BLACK, (2, 4), '-', '5', (4, 5)),
+
+        (ELEPHANT, RED, (2, 0), '+', '9', (0, 2)),
+        (ELEPHANT, RED, (2, 0), '+', '5', (4, 2)),
+        (ELEPHANT, BLACK, (2, 9), '+', '1', (0, 7)),
+        (ELEPHANT, BLACK, (2, 9), '+', '5', (4, 7)),
+        (ELEPHANT, RED, (2, 4), '-', '9', (0, 2)),
+        (ELEPHANT, RED, (2, 4), '-', '5', (4, 2)),
+        (ELEPHANT, BLACK, (2, 5), '-', '1', (0, 7)),
+        (ELEPHANT, BLACK, (2, 5), '-', '5', (4, 7)),
+
+        (ADVISOR, RED, (4, 1), '+', '6', (3, 2)),
+        (ADVISOR, RED, (4, 1), '+', '4', (5, 2)),
+        (ADVISOR, BLACK, (4, 8), '+', '4', (3, 7)),
+        (ADVISOR, BLACK, (4, 8), '+', '6', (5, 7)),
+        (ADVISOR, RED, (4, 1), '-', '6', (3, 0)),
+        (ADVISOR, RED, (4, 1), '-', '4', (5, 0)),
+        (ADVISOR, BLACK, (4, 8), '-', '4', (3, 9)),
+        (ADVISOR, BLACK, (4, 8), '-', '6', (5, 9)),
+
+        (KING, RED, (4, 1), '+', '1', (4, 2)),
+        (KING, BLACK, (4, 8), '+', '1', (4, 7)),
+        (KING, RED, (4, 1), '-', '1', (4, 0)),
+        (KING, BLACK, (4, 8), '-', '1', (4, 9)),
+        (KING, RED, (4, 1), '.', '4', (5, 1)),
+        (KING, RED, (4, 1), '.', '6', (3, 1)),
+        (KING, BLACK, (4, 8), '.', '4', (3, 8)),
+        (KING, BLACK, (4, 8), '.', '6', (5, 8)),
+
+        (PAWN, RED, (2, 5), '+', '1', (2, 6)),
+        (PAWN, BLACK, (2, 4), '+', '1', (2, 3)),
+        (PAWN, RED, (2, 5), '.', '8', (1, 5)),
+        (PAWN, RED, (2, 5), '.', '6', (3, 5)),
+        (PAWN, BLACK, (2, 4), '.', '2', (1, 4)),
+        (PAWN, BLACK, (2, 4), '.', '4', (3, 4)),
+
+        (CANNON, RED, (3, 5), '+', '3', (3, 8)),
+        (CANNON, BLACK, (3, 5), '+', '3', (3, 2)),
+        (CANNON, RED, (3, 5), '-', '3', (3, 2)),
+        (CANNON, BLACK, (3, 5), '-', '3', (3, 8)),
+        (CANNON, RED, (3, 5), '.', '1', (8, 5)),
+        (CANNON, RED, (3, 5), '.', '9', (0, 5)),
+        (CANNON, BLACK, (3, 5), '.', '1', (0, 5)),
+        (CANNON, BLACK, (3, 5), '.', '9', (8, 5)),
+    ])
+    def test_calculate_destination_of_move(self, piece, party, cur_x_y, operator, argument, expected_new_x_y):
+        put_piece = PutPiece(party, piece, *cur_x_y)
+        actual_new_x_y = _Board._calculate_destination_of_move(put_piece, operator, argument)
+        self.assertEqual(actual_new_x_y, expected_new_x_y)
